4558ea9e159ed167a705cb7fe7afbf44d475eb1d
Lincoln Jose Alvares Irano <lincoln.jairano@gmail.com>
2021-04-04 11:10:21 -0300

    Adding some configs and stuffs

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.mdx",
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/components/**/stories.mdx',
+    '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-  ]
+  addons: [
+    '@storybook/addon-essentials',
+  ],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
